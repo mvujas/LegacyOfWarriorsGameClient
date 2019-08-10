04cd343bd8252d258f2f532f8e36ff11f60b1e2e
Milos <milos.vujasinovic.98@gmail.com>
2019-08-10 22:31:17 +0200

    9 cards added

--- LegacyOfWarriors/Assets/Scripts/SceneScripts/HomeScene/Home/HomeScreenLogic.cs
+++ LegacyOfWarriors/Assets/Scripts/SceneScripts/HomeScene/Home/HomeScreenLogic.cs
@@ -86,7 +86,7 @@ public class HomeScreenLogic : TemporarySimpleGUIComponent
     {
         globalReference.GameClient.Send(new QueueEntryRequest
         {
-            Deck = new int[] { 0, 0, 0, 0, 1, 1, 1 }
+            Deck = new int[] { 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 6, 6, 7, 8 }
         });
     }
 
